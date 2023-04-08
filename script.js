4bc8747e426d7b42dc09564f71d8154e91547988
Abhishek Kumar <76006019+allworkdone@users.noreply.github.com>
2023-04-08 11:39:13 +0530

    JS : js Script configurations Most of the page actions are created in the javascript file all the codes related to js customization are here.

---
+++ script.js
@@ -0,0 +1,126 @@
+
+// Fetching JS Properties
+const inputSlider = document.querySelector("[data-lenghtSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numberCheck = document.querySelector("#numbers");
+const symbolCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+
+handleSlider();
+// Set strength circle color to grey
+
+
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color) {
+    indicator.style.backgroundColor = color;
+    // shadow
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomNumber() {
+    return getRndInteger(0, 9);
+}
+
+function generateLowerCase() {
+    return String.fromCharCode(getRndInteger(97, 123));
+}
+
+function generateUpperCase() {
+    return String.fromCharCode(getRndInteger(65, 91));
+}
+
+function generateSymbol() {
+    const rndNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(rndNum);
+}
+
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolCheck.checked) hasSym = true;
+
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+        setIndicator("#0f0");
+    }
+    else if (
+        (hasLower || hasUpper) &&
+        (hasNum || hasSym) &&
+        passwordLength >= 6
+    ) {
+        setIndicator("#ff0");
+    }
+    else {
+        setIndicator("#f00");
+    }
+}
+
+async function copyContent() {
+    try {
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText = "copied";
+    }
+    catch (e) {
+        copyMsg.innerText = "failed";
+    }
+
+    copyMsg.classList.add("active");
+
+    setTimeout(() => {
+        copyMsg.classList.remove("active");
+    }, 2000);
+}
+
+function handleCheckBoxChange() {
+    checkCount = 0;
+    allCheckBox.forEach((checkbox) => {
+        if (checkbox.checked) checkCount++;
+    });
+
+    // special condition
+    if (passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+}
+
+allCheckBox.forEach((checkbox) => {
+    checkbox.addEventListener('change', handleCheckBoxChange);
+});
+
+inputSlider.addEventListener('input', (e) => {
+    passwordLength = e.target.value;
+    handleSlider();
+});
+
+copyBtn.addEventListener('click', () => {
+    if (passwordDisplay.value) copyContent();
+});
+
+generateBtn.addEventListener('click', () => {
+
+});
